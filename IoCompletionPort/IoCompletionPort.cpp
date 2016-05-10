c543d992c3e084d64a56f73752374eaee5e6fb46
JackieLee <992065705@qq.com>
2016-05-10 19:19:33 +0800

    完成端口模型服务器代码

--- IoCompletionPort/IoCompletionPort.cpp
+++ IoCompletionPort/IoCompletionPort.cpp
@@ -18,7 +18,7 @@ typedef struct
 typedef struct
 {
 	SOCKET Socket;
-} PER_HANDLE_DATA,*LPPER_HANDLE_DATA;
+} PER_HANDLE_DATA, *LPPER_HANDLE_DATA;
 DWORD WINAPI ServerWorkerThread(LPVOID CompletionPortID)
 {
 	HANDLE CompletionPort = (HANDLE)CompletionPortID;
@@ -86,7 +86,7 @@ DWORD WINAPI ServerWorkerThread(LPVOID CompletionPortID)
 			ZeroMemory(&(PerIoData->Overlapped), sizeof(Overlapped));
 			PerIoData->DataBuf.len = DATA_BUFSIZE;
 			PerIoData->DataBuf.buf = PerIoData->Buffer;
-			if (WSARecv(PerHandleData->Socket, &(PerIoData->DataBuf), 
+			if (WSARecv(PerHandleData->Socket, &(PerIoData->DataBuf),
 				1, &RecvBytes, &Flags, &(PerIoData->Overlapped), NULL) == SOCKET_ERROR)
 			{
 				if (WSAGetLastError() != ERROR_IO_PENDING)
@@ -98,10 +98,106 @@ DWORD WINAPI ServerWorkerThread(LPVOID CompletionPortID)
 		}
 	}
 }
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("I have changed!\n");
-	printf("I have changed repository!\n");
+	SOCKADDR_IN InternetAddr;
+	SOCKET Listen;
+	SOCKET Accept;
+	HANDLE CompletionPort;
+	SYSTEM_INFO SystemInfo;
+	LPPER_HANDLE_DATA PerHandleData;
+	LPPER_IO_OPERATION_DATA PerIoData;
+	DWORD RecvBytes;
+	DWORD Flags;
+	DWORD ThreadID;
+	WSADATA wsaData;
+	DWORD Ret;
+
+	if ((Ret = WSAStartup(0x0202, &wsaData)) != 0){
+		printf("WSAStartup failed!\n");
+		return -1;
+	}
+
+	//创建新的完成端口
+	if ((CompletionPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0)) == NULL)
+	{
+		printf("CreateCompletionPort failed!\n");
+		return -1;
+	}
+
+	GetSystemInfo(&SystemInfo);
+
+	for (int i = 0; i < SystemInfo.dwNumberOfProcessors * 2; i++)
+	{
+		HANDLE ThreadHandle;
+		if ((ThreadHandle = CreateThread(NULL, 0, ServerWorkerThread, CompletionPort, 0, &ThreadID)) == NULL)
+		{
+			printf("CreateThread falied!\n");
+			return -1;
+		}
+		CloseHandle(ThreadHandle);
+	}
+
+	if ((Listen = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED)) == INVALID_SOCKET)
+	{
+		printf("WSASocket failed!\n");
+		return -1;
+	}
+	//绑定到本地地址
+	InternetAddr.sin_family = AF_INET;
+	InternetAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	InternetAddr.sin_port = htons(PORT);
+	if (bind(Listen, (PSOCKADDR)&InternetAddr, sizeof(InternetAddr)) == SOCKET_ERROR)
+	{
+		printf("bind failed!\n");
+		return -1;
+	}
+	if (listen(Listen, 5) == SOCKET_ERROR)
+	{
+		printf("listen faild!\n");
+		return -1;
+	}
+	while (true)
+	{
+		if ((Accept = WSAAccept(Listen, NULL, NULL, NULL, 0)) == SOCKET_ERROR)
+		{
+			printf("WSAAccept failed!\n");
+			return -1;
+		}
+		if ((PerHandleData = (LPPER_HANDLE_DATA)GlobalAlloc(GPTR, sizeof(PER_HANDLE_DATA))) == NULL)
+		{
+			printf("GlobalAlloc failed!\n");
+			return -1;
+		}
+		PerHandleData->Socket = Accept;
+		if (CreateIoCompletionPort((HANDLE)Accept, CompletionPort, (DWORD)PerHandleData, 0) == NULL)
+		{
+			printf("CreateIoCompletionport!\n");
+			return -1;
+		}
+		//为io结构体分配内存空间
+		if ((PerIoData = (LPPER_IO_OPERATION_DATA)GlobalAlloc(GPTR, sizeof(PER_IO_OPERATION_DATA))) == NULL)
+		{
+			printf("GlobalAlloc failed!\n");
+			return -1;
+		}
+		ZeroMemory(&(PerIoData->Overlapped), sizeof(OVERLAPPED));
+		PerIoData->BytesSEND = 0;
+		PerIoData->BytesRECV = 0;
+		PerIoData->DataBuf.len = DATA_BUFSIZE;
+		PerIoData->DataBuf.buf = PerIoData->Buffer;
+		Flags = 0;
+
+		if (WSARecv(Accept, &(PerIoData->DataBuf), 1, &RecvBytes, &Flags, &(PerIoData->Overlapped), NULL) == SOCKET_ERROR)
+		{
+			if (WSAGetLastError() != ERROR_IO_PENDING)
+			{
+				printf("WSARecv() failed!\n");
+				return -1;
+			}
+		}
+	}
 	return 0;
 }
 
